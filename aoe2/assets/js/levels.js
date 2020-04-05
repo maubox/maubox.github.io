a3c70cda97f31cd638cec3c1f8bc097633333053
maubox <motencu@gmail.com>
2020-04-05 15:35:47 -0500

    add aoe2

---
+++ aoe2/assets/js/levels.js
@@ -0,0 +1,209 @@
+var leveler;
+const all = [{
+        name: "archery",
+        code: 1,
+        painting: "/assets/images/aoe/archery.png"
+    },
+    {
+        code: 2,
+        painting: "/assets/images/aoe/barracks.png"
+    },
+    {
+        code: 3,
+        painting: "/assets/images/aoe/stable.png"
+    },
+    {
+        code: 4,
+        painting: "/assets/images/aoe/seige.png"
+    },
+    {
+        name: "idle village",
+        code: 5,
+        painting: "/assets/images/aoe/idle.png"
+    },
+    {
+        name: "tc",
+        code: 6,
+        painting: "/assets/images/aoe/tc.png"
+    },
+    {
+        name: "tc",
+        code: 7,
+        painting: "/assets/images/aoe/mill.png"
+    },
+    {
+        name: "tc",
+        code: 8,
+        painting: "/assets/images/aoe/blacksmith.png"
+    },
+    {
+        name: "tc",
+        code: 9,
+        painting: "/assets/images/aoe/market.png"
+    },
+    {
+        name: "tc",
+        code: 10,
+        painting: "/assets/images/aoe/monastary.png"
+    },
+    {
+        name: "tc",
+        code: 11,
+        painting: "/assets/images/aoe/university.png"
+    },
+    {
+        name: "tc",
+        code: 12,
+        painting: "/assets/images/aoe/castle.png"
+    }
+];
+
+if (level === 1) {
+    leveler = [{
+            code: 3,
+            painting: "/assets/images/aoe/stable.png"
+        },
+        {
+            name: "idle village",
+            code: 5,
+            painting: "/assets/images/aoe/idle.png"
+        },
+        {
+            name: "tc",
+            code: 6,
+            painting: "/assets/images/aoe/tc.png"
+        },
+    ]
+} else if (level === 2) {
+    leveler = [{
+            name: "archery",
+            code: 1,
+            painting: "/assets/images/aoe/archery.png"
+        },
+        {
+            code: 2,
+            painting: "/assets/images/aoe/barracks.png"
+        },
+        {
+            code: 3,
+            painting: "/assets/images/aoe/stable.png"
+        },
+        {
+            code: 4,
+            painting: "/assets/images/aoe/seige.png"
+        },
+        {
+            name: "idle village",
+            code: 5,
+            painting: "/assets/images/aoe/idle.png"
+        },
+        {
+            name: "tc",
+            code: 6,
+            painting: "/assets/images/aoe/tc.png"
+        },
+    ]
+} else if (level === 3) {
+    leveler = [{
+            name: "archery",
+            code: 1,
+            painting: "/assets/images/aoe/archery.png"
+        },
+        {
+            code: 2,
+            painting: "/assets/images/aoe/barracks.png"
+        },
+        {
+            code: 3,
+            painting: "/assets/images/aoe/stable.png"
+        },
+        {
+            code: 4,
+            painting: "/assets/images/aoe/seige.png"
+        },
+        {
+            name: "idle village",
+            code: 5,
+            painting: "/assets/images/aoe/idle.png"
+        },
+        {
+            name: "tc",
+            code: 6,
+            painting: "/assets/images/aoe/tc.png"
+        },
+        {
+            name: "tc",
+            code: 7,
+            painting: "/assets/images/aoe/mill.png"
+        },
+        {
+            name: "tc",
+            code: 8,
+            painting: "/assets/images/aoe/blacksmith.png"
+        },
+        {
+            name: "tc",
+            code: 9,
+            painting: "/assets/images/aoe/market.png"
+        }
+    ]
+} else if (level === 4) {
+    leveler = [{
+            name: "archery",
+            code: 1,
+            painting: "/assets/images/aoe/archery.png"
+        },
+        {
+            code: 2,
+            painting: "/assets/images/aoe/barracks.png"
+        },
+        {
+            code: 3,
+            painting: "/assets/images/aoe/stable.png"
+        },
+        {
+            code: 4,
+            painting: "/assets/images/aoe/seige.png"
+        },
+        {
+            name: "idle village",
+            code: 5,
+            painting: "/assets/images/aoe/idle.png"
+        },
+        {
+            name: "tc",
+            code: 6,
+            painting: "/assets/images/aoe/tc.png"
+        },
+        {
+            name: "tc",
+            code: 7,
+            painting: "/assets/images/aoe/mill.png"
+        },
+        {
+            name: "tc",
+            code: 8,
+            painting: "/assets/images/aoe/blacksmith.png"
+        },
+        {
+            name: "tc",
+            code: 9,
+            painting: "/assets/images/aoe/market.png"
+        },
+        {
+            name: "tc",
+            code: 10,
+            painting: "/assets/images/aoe/monastary.png"
+        },
+        {
+            name: "tc",
+            code: 11,
+            painting: "/assets/images/aoe/university.png"
+        },
+        {
+            name: "tc",
+            code: 12,
+            painting: "/assets/images/aoe/castle.png"
+        }
+    ]
+}
